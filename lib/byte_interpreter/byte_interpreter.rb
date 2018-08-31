1d1343394480f6f01e6e2076c7352acbb4708b82
DESKTOP-UBRRAMU\Michael <42737424+mkgremillion@users.noreply.github.com>
2018-08-31 09:40:45 -0500

    Write documentation
    Document the classes and methods, explain how to use the interpreter, and
    explain how to extend it with your own load methods.
    
    Fixes issues #1 and #4

--- lib/byte_interpreter/byte_interpreter.rb
+++ lib/byte_interpreter/byte_interpreter.rb
@@ -1,22 +1,61 @@
 # frozen_string_literal: true
 
-require './Instructions.rb'
+require './byte_interpreter/instructions.rb'
 
 ##
 # The ByteInterpreter is a tool used to extract bytes and strings from a binary
 # file, and also to encode bytes and strings into a binary file. It can also
 # take a series of instructions to extract or encode data in an ordinal manner,
-# suitable for writing binary files with strict structure size requirements.
+# suitable for writing binary files with rigid structure size requirements.
 class ByteInterpreter
 
+  ##
+  # Reads the endian mode being used by the interpreter.
   attr_reader :endian_mode
 
-  def initialize(endian: nil, iostream:)
+  ##
+  # Creates and sets up a new ByteInterpreter.
+  # @param endian [:little, :big, nil] Default for this value is nil. The
+  #   endian mode that will be used by the interpreter for reading/writing
+  #   bytes. If nil is specified, the interpreter will assume machine-native
+  #   endianness.
+  # @param stream [#read, #write] The IO stream, or IO-like object, that the
+  #   interpreter will perform operations on. The interpreter will not open or
+  #   close the stream for you, and assumes you have already changed the
+  #   position to the appropriate offset for its operations. The interpreter
+  #   also assumes you have opened the stream as binary (as opposed to text),
+  #   and for the appropriate operations (read/write).
+  def initialize(endian: nil, stream:)
     @endian_mode = endian.to_sym
-    @iostream = iostream
     @instructions = nil
+    iostream = stream
   end
 
+  ##
+  # Changes the stream being used by the interpreter for operations.
+  # @param new_stream [#read, #write] The IO stream, or IO-like object, that
+  #   the interpreter will perform operations on. See #new for what is expected
+  #   of this stream.
+  # @return [ByteInterpreter] self
+  def iostream=(new_stream)
+    if stream_like?(obj: new_stream)
+      @iostream = new_stream if stream_like?(obj: new_stream)
+    else
+      raise ArgumentError.new("Object given is not stream-like.")
+    end
+
+    self
+  end
+
+  ##
+  # Reads a set number of bytes, interprets them into an integer, and returns
+  # the result.
+  # @param size [1, 2, 4, 8] The number of bytes to read from the stream.
+  #   ByteInterpreter can only interpret 8-, 16-, 32-, and 64-bit values at
+  #   this time, so this parameter is limited to just a few numbers.
+  # @param signed [Boolean] Default for this value is false. Set this to
+  #   true if the bytes being read can be negative or positive.
+  # @return [Integer] the interpreted byte
   def interpret_bytes(size: 2, signed: false)
     bytes = @iostream.read(size)
     directive = build_directive(size: size, signed: signed)
@@ -24,10 +63,26 @@ class ByteInterpreter
     bytes.unpack(directive).first
   end
 
+  ##
+  # Reads a set number of bytes, interprets them into a string, and returns the
+  # result.
+  # @param size [Integer] The number of bytes to read from the stream. Unlike
+  #   #interpret_bytes, this size can be any positive integer.
+  # @return [String] the interpreted string
   def interpret_string(size: )
     @iostream.read(size)
   end
 
+  ##
+  # Writes a set number of bytes, encoded from the given value.
+  # @param value [Integer] The value to encode and write.
+  # @param size [1,2,4,8] The size of the value in bytes.
+  # @param signed [Boolean] Set this to true if the bytes being written can be
+  #   negative and positive, false otherwise.
+  # @return [void]
+  # @note The interpreter makes no attempt to ensure that your +value+ fits
+  #   into +size+ bytes. To avoid unintended behavior, you should validate your
+  #   input into this method.
   def encode_bytes(value:, size:, signed:)
     unless value.respond_to? :pack
       value = Array(value)
@@ -35,16 +90,44 @@ class ByteInterpreter
     @iostream.write(value.pack(build_directive(size: size, signed: signed)))
   end
 
+  ##
+  # Writes a string into a given number of bytes.
+  # @param value [String] The value to write to the stream.
+  # @param size [Integer] The size of the value in bytes. Unlike
+  #   #encode_bytes, this size can be any positive integer.
+  # @return [void]
+  # @note If +value+ is smaller than +size+, the interpreter will pad +value+
+  #   with 0x20 to fill the remaining space. Even so, care should be taken to
+  #   validate your input to this method, especially if you want to handle
+  #   strings that are larger than +size+, or want to handle size differences
+  #   differently than this method.
   def encode_string(value:, size:)
     @iostream.write(value.ljust(size, "\x20"))
   end
 
+  ##
+  # Loads instructions from a file for structured, ordinal operations.
+  # @param type [Symbol] The type of the file that holds the instructions.
+  #   This argument **must** have a corresponding method in the
+  #   ByteInterpreter::Instructions class, named +load_from_type+, replacing
+  #   +type+ with the actual name of the filetype.
+  # @param filename [String] The filename of the instructions to load.
+  # @return [void]
+  # @note ByteInterpreter comes with only one +type+ built-in: JSON.
   def load_instructions(type:, filename:)
     @instructions = Instructions.new if @instructions.nil?
     @instructions.clear
     @instructions.send("load_from_" + type.to_s, filename: filename)
   end
 
+  ##
+  # Uses the loaded instructions (you did call #load_instructions first,
+  # right?) to interpret bytes and strings from the stream, passing them as
+  # arguments to the given block.
+  # @yieldparam key [Symbol] The key of the interpreted data. Typically used to
+  #   set variables in the calling object.
+  # @yieldparam value [Integer, String] The value of the interpreted data.
+  # @return [Integer] the combined size, in bytes, of the operation
   def interpret_from_instructions
     struct_size = 0
     @instructions.each do |field|
@@ -62,6 +145,18 @@ class ByteInterpreter
     return struct_size
   end
 
+  ##
+  # Uses the loaded instructions (you did call #load_instructions first,
+  # right?) to encode the given values into bytes and strings, and write them
+  # to the stream.
+  #
+  # This method encodes and writes bytes in the order of the loaded
+  # instructions; this means it will seek each key from the given Hash, instead
+  # of seeking around the file and writing in whatever order the Hash may be
+  # in.
+  # @param values [Hash] The values to read and encode. This Hash **must**
+  #   have keys that match *all* keys from the loaded instructions.
+  # @return [Integer] the combined size, in bytes, of the operation
   def encode_from_instructions(values:)
     struct_size = 0
     @instructions.each do |field|
@@ -82,16 +177,29 @@ class ByteInterpreter
 
   private
 
+  ##
+  # This constant maps byte lengths to their respective Strings for the
+  # directives in Array#pack and String#unpack.
   DIRECTIVE_SIZES = {1 => "C", 2 => "S", 4 => "L", 8 => "Q"}
 
+  ##
+  # Uses DIRECTIVE_SIZES to translate a byte length to a usable String.
+  # @param size [1,2,4,8] The byte length to translate.
+  # @raise [ArgumentError] if +size+ is not 1, 2, 4, or 8.
+  # @return [String] the translated directive String.
   def determine_directive_letter(size:)
     if DIRECTIVE_SIZES.has_key?(size)
-      return DIRECTIVE_SIZES[size]
+      return DIRECTIVE_SIZES[size].dup
     else
       raise ArgumentError.new("Invalid size argument (#{size}). See documentation for valid sizes.")
     end
   end
 
+  ##
+  # Returns the glyph for the set endianness, for use in building the directive
+  # String.
+  # @return [String] if endian_mode is non-nil
+  # @return [nil] if endian_mode is nil
   def determine_endian_glyph
     case endian_mode
     when :little
@@ -101,6 +209,12 @@ class ByteInterpreter
     end
   end
 
+  ##
+  # Builds a directive String, fit for use in Array#pack and String#unpack.
+  # @param size [1,2,4,8] The size to translate into a directive String.
+  # @param signed [Boolean] Set this to true if the bytes being written can be
+  #   negative and positive, false otherwise.
+  # @return [String] the built directive String
   def build_directive(size: , signed:)
     directive = determine_directive_letter(size: size)
     directive.downcase! if signed
@@ -110,6 +224,17 @@ class ByteInterpreter
     end
 
     directive
+  end
+
+  ##
+  # Checks if the given object is stream-like -- that is, responds to #read
+  # and #write.
+  # @param obj [Object] The object to test.
+  # @return [Boolean]
+  # @note For fun, consider making an inverse of this method named
+  #   "illiterate?"
+  def stream_like?(obj:)
+    return obj.respond_to?(:read) && obj.respond_to?(:write)
   end
 
 end
--- lib/byte_interpreter/byte_interpreter/instructions.rb
+++ lib/byte_interpreter/byte_interpreter/instructions.rb
@@ -1,33 +1,87 @@
 #frozen_string_literal: true
 
+require 'json'
+
 class ByteInterpreter
 
+  ##
+  # The Instructions class represents a collection of ordered operations to
+  # perform on an IO stream. This class is used by ByteInterpreter to either
+  # interpret or encode bytes in a rigid, structured way.
+  #
+  # At the most basic level, Instructions are just an Array filled with Hashes,
+  # each Hash having exactly four keys -- :key, :type, :size, and :signed. Each
+  # key has requirements for its value:
+  # - :key -- Must be a value easily convertible to a Symbol object.
+  # - :type -- Must match one of the elements in the constant VALID_TYPES.
+  # - :size -- For binary types ("bin"), must match one of the elements in the
+  #   constant VALID_BIN_SIZES. String types ("str") must be a
+  #   positive Integer.
+  # - :signed -- For binary types ("bin"), must be the +true+ or +false+
+  #   literals. String types ("str") ignore this value completely.
+  #
+  # Writing your own method for loading instructions is fairly simple. The
+  # method must call #add_field in the desired order of instruction execution,
+  # passing to it a Hash that conforms to the requirements above. The method
+  # must also be named +load_from_type+, where +type+ is what will be
+  # passed into ByteInterpreter#load_instructions. See #load_from_json for an
+  # example of this.
   class Instructions
 
+    ##
+    # Raised by instruction validation methods.
     class ValidationError < StandardError
     end
 
+    ##
+    # Valid values for the :type key in the instructions Hash.
     VALID_TYPES = ["bin", "str"]
+
+    ##
+    # Valid values for binary types for the :size key in the instructions Hash.
     VALID_BIN_SIZES = [1, 2, 4, 8]
+
+    ##
+    # Keys that are in every properly-formatted instructions Hash.
     FIELD_NAMES = [:key, :type, :size, :signed]
 
+    ##
+    # Creates a blank Instructions object.
     def initialize
       @data = []
     end
 
+    ##
+    # Passes the given block to the internal Array's #each method.
     def each(&block)
       @data.each(&block)
     end
 
+    ##
+    # Clears all loaded instructions.
     def clear
       @data.clear
     end
 
+    ##
+    # Adds the given Hash to the end of the list of instructions, and validates
+    # it.
+    # @param field [Hash] A properly-formatted instructions Hash. See the
+    #   documentation for this class on the appropriate format for this Hash.
+    # @return [void]
     def add_field(field:)
       @data.push(field.select {|k,v| FIELD_NAMES.include? k})
       validate_field(field: @data.last)
     end
 
+    ##
+    # Loads instructions from a JSON file. The JSON file should contain a
+    # top-level array, with each element being an object with the appropriate
+    # keys and values. Keys are automatically converted from strings to
+    # symbols, but boolean values are not converted from strings to literals.
+    # @param filename [String] The filename of the JSON file to load,
+    #   **including** the filetype extension, if any.
+    # @return [void]
     def load_from_json(filename:)
       json_fields = JSON.parse(File.open(filename, "rt") {|file| file.read}, {:symbolize_names => true})
       json_fields.each do |field|
@@ -35,6 +89,12 @@ class ByteInterpreter
       end
     end
 
+    ##
+    # Validates a given Hash to ensure it conforms to the instruction format.
+    # @param field [Hash] The Hash object to evaluate.
+    # @return [Boolean]
+    # @raise [ValidationError] if the Hash does not conform to the instruction
+    #   format
     def validate_field(field:)
       unless VALID_TYPES.include? field[:type]
         raise ValidationError, "Illegal type defined at key \"#{field[:key]}\": #{field[:type]}. Valid types are #{VALID_TYPES}."
